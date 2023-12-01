22e9fd7160db49e4a12c4223486231f4565476b6
Nildo Galvino <94250281+nildogalvino@users.noreply.github.com>
2023-12-01 15:45:32 -0300

    Curso JavaScript Matheus Fraga
    Primeira aula do curso do Matheus Fraga

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+var nome = prompt("Qual o seu nome");
+
+var sobrenome = prompt("Digite o seu sobre nome");
+
+console.log("Olá " + nome + " " + sobrenome);
+
+document.write("<h1>Bem vindo " + nome + " </h1>");
+
+document.write("<img src='https://sujeitoprogramador.com/steve.png' alt='Foto do Steve' />")
