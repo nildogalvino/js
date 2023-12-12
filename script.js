ce800d2d72e026509a762e51b29a41448da5ab2c
Nildo Galvino <94250281+nildogalvino@users.noreply.github.com>
2023-12-12 07:53:41 -0300

    Curso Matheus Fraga
    entendendo um pouco mais, sobre o JavaScript

--- script.js
+++ script.js
@@ -20,3 +20,21 @@ function sair(){
     alert("Ate mais!");
     area.innerHTML = "Você saiu";
 }
+
+function mediaAluno(nota1, nota2){
+
+    var media = (nota1 + nota2) / 2;
+
+    if( media >= 7){
+      console.log("Aluno aprovado coma média: " + media)
+    }else if(media < 7){
+        console.log("Aluno reprovado com a média: " + media)
+    }
+}
+
+function aluno (nome, curso){
+
+    var mensagem = 'Seja bem vindo ' + nome + ' ao curso de ' + curso;
+
+    console.log(mensagem);
+}
