de2ac34f27720b0f8165eb6b6a3013abae9a30df
Nildo Galvino <94250281+nildogalvino@users.noreply.github.com>
2023-12-13 08:12:59 -0300

    Curso Matheus Fraga
    Corrigindo o projeto, e deixando o código um pouco mais limpo!

--- script.js
+++ script.js
@@ -7,13 +7,13 @@
 
  // peso / (altura * altura);
 
-var peso;
-var altura;
-var imc;
-var resultado;
+ var peso;
+ var altura;
+ var imc;
+ var resultado;
 
 
- function calcular(event){
+function calcular(event){
   event.preventDefault();
 
   peso = document.getElementById('peso').value;
@@ -24,14 +24,14 @@ var resultado;
   resultado = document.getElementById('resultado');
 
   if(imc < 17){
-    resultado.innerHTML = '<br/> Seu resultado foi: ' + imc.toFixed(2) + '<br/> Cuidado você está muito abaixo do peso!';
-  }else if(imc > 17 && imc <= 18.99){
-    resultado.innerHTML = '<br/> Seu resultado foi: ' + imc.toFixed(2) + '<br/> Você esta baixo do peso!';
+    resultado.innerHTML = '<br> Seu resultado foi: ' + imc.toFixed(2) + '<br> Cuidado você está muito abaixo do peso!'
+  }else if(imc > 17 && imc <= 18.49){
+    resultado.innerHTML = '<br> Seu resultado foi: ' + imc.toFixed(2) + '<br> Você está abaixo do peso!'
   }else if(imc >= 18.5 && imc < 24.99){
-    resultado.innerHTML = '<br/> Seu resultado foi: ' + imc.toFixed(2) + '<br/> Você esta baixo do peso!';
-  }else if(imc > 25 && imc <=  29,99){
-    resultado.innerHTML = '<br/> Seu resultado foi: ' + imc.toFixed(2) + '<br/> Cuidado você esta acima do peso!';
+    resultado.innerHTML = '<br> Seu resultado foi: ' + imc.toFixed(2) + '<br> Você está no peso ideal!'
+  }else if(imc > 25 && imc <= 29.99){
+    resultado.innerHTML = '<br> Seu resultado é: ' + imc.toFixed(2) + '<br> Cuidado você está acima do peso!'
   }else if(imc >= 30){
-    resultado.innerHTML = '<br/> Seu resultado foi: ' + imc.toFixed(2) + '<br/> Você esta na obesidade!';
+    resultado.innerHTML = '<br> Seu resultado é : ' + imc.toFixed(2) + ' <br> Você está com obesidade!'
   }
 }
